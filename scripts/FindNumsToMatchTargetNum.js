ee05d5fa301c5a03a53919d3d7d62150d719883b
wonma <wonmadesign@gmail.com>
2022-07-03 13:25:21 -0500

    Solved 'Find two numbers that sum up to the target number' logic

--- scripts/FindNumsToMatchTargetNum.js
+++ scripts/FindNumsToMatchTargetNum.js
@@ -18,3 +18,34 @@ For example: answer([1,2,3], 4)should return [1,3]
   };
 })();
 /* ------------------------------------------------ */
+
+/* --------------- Take two arguments ----------------
+1. const findTwo = (arr, target) => {
+
+}
+2.  [1,2,3,15,20,145,2000]에서 두개를 찾아서 2001을 만들어야함.
+  
+for (startingNumber=0; startingNumber < arr.length -1; startingNumber++) {
+  startiingNumber본인과 본인다음 (본인 + 1) index를 더하고, (처음은 본인0 ~ <  arr.length 까지고 )
+  startingnumber본인과 본인다다음 본인 + 2 nidex를 더하고, (그다음은 본인1 ~ <  arr.length -1 까지고 )
+
+}
+*/
+const myArray = [1, 2, 3, 15, 20, 145, 2000];
+
+const findTwo = (arr, target) => {
+  for (let index = 0; index < arr.length; index++) {
+    let targetIndex = 1;
+    let lastIndex = arr.length;
+    let num1 = arr[index];
+    let num2 = arr[index + targetIndex];
+    while (targetIndex < lastIndex) {
+      num2 = arr[targetIndex];
+      if (num1 + num2 === target) {
+        return `The two numbers that make a total of ${target} are ${num1} and ${num2}.`;
+      }
+      targetIndex++;
+    }
+  }
+};
+console.log(findTwo(myArray, 35));
